3218e9154a16c0da585f89116cf2b3dd4c46b0bc
Spenny Berks <sberkebile@live.com>
2021-07-23 23:19:50 -0400

    fixed db errors for placeholder data

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,7 +16,7 @@ VALUES
     ("Support Lead", 900000, 2),
     ("Technician", 460000, 2),
     ("Salesperson", 860000, 3),
-    ("Sales Lead", 12000000, 3),
+    ("Sales Lead", 1200000, 3),
     ("Accountant", 860000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -24,12 +24,12 @@ VALUES
     ("Spencer", "Berkebile", 1, 2),
     ("Hayley", "Harris", 2, NULL),
     ("Anthony", "Ciavarella", 3, NULL),
-    ("Caitlin", "Noone", 3, NULL);
-    ("John", "Doe", 4, 1);
-    ("Jackson", "Pollack", 4, 1);
-    ("Frankie", "Mack", 5, 8);
-    ("Jamie", "Banders", 6, NULL);
-    ("Jason", "Crow", 7, 8);
-    ("Gabe", "Drowson", 8, 11);
-    ("Elizabeth", "Lenner", 9, NULL);
+    ("Caitlin", "Noone", 3, NULL),
+    ("John", "Doe", 4, 1),
+    ("Jackson", "Pollack", 4, 1),
+    ("Frankie", "Mack", 5, 8),
+    ("Jamie", "Banders", 6, NULL),
+    ("Jason", "Crow", 7, 8),
+    ("Gabe", "Drowson", 8, 11),
+    ("Elizabeth", "Lenner", 9, NULL),
     ("Paris", "London", 10, NULL);
