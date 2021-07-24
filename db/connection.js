d14d0e95dd23e51ca9c89baafa799860893fb0fc
Spenny Berks <sberkebile@live.com>
2021-07-23 22:00:59 -0400

    development of app.js

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,3 @@
-const mysql = require('mysql2');
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'H@yley=1',
-        database: 'employeetracker'
-    },
-    console.log('Connected to the employee tracker database')
-);
 
 module.exports = db;
---
+++ db/seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO department (name)
+VALUES
+    ("Operational Support"),
+    ("Desktop Support"),
+    ("Sales"),
+    ("Finance");
+    
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Support Lead", 900000, 1),
+    ("Software Engineer", 1000000, 1),
+    ("Analyst", 770000, 1),
+    ("Coordinator", 420000, 1),
+    ("Product Manager", 850000, 2),
+    ("Support Lead", 900000, 2),
+    ("Technician", 460000, 2),
+    ("Salesperson", 860000, 3),
+    ("Sales Lead", 12000000, 3),
+    ("Accountant", 860000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Spencer", "Berkebile", 1, 2),
+    ("Hayley", "Berkebile", 2, NULL),
+    ("Anthony", "Ciavarella", 3, NULL),
+    ("Caitlin", "Noone", 3, NULL);
+    ("John", "Doe", 4, 1);
+    ("Jackson", "Pollack", 4, 1);
+    ("Frankie", "Mack", 5, 8);
+    ("Jamie", "Banders", 6, NULL);
+    ("Jason", "Crow", 7, 8);
+    ("Gabe", "Drowson", 8, 11);
+    ("Elizabeth", "Lenner", 9, NULL);
+    ("Paris", "London", 10, NULL);
